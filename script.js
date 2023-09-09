f9af7363fdb797a6ae552830938469f36663dd62
Birphon <61985658+Birphon@users.noreply.github.com>
2023-09-09 19:42:18 +1200

    it be kinda working

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-// script.js
-let videoId = "videoId"; // Default value - Thought this would fix the null src
-
-function parseLink() {
-	const inputElement = document.getElementById("youtubeLink");
-	const inputValue = inputElement.value;
-
-	const prefix = "https://www.youtube.com/watch?v=";
-	if (inputValue.startsWith(prefix)) {
-		videoId = inputValue.substring(prefix.length);
-		console.log("Video ID:", videoId);
-
-		// Update the video iframe src with the updated videoId
-		const videoFrame = document.getElementById("videoFrame");
-		videoFrame.src = `https://www.youtube.com/embed/${videoId}`;
-
-		// Update the live chat iframe src with the updated videoId
-		const liveChatFrame = document.getElementById("liveChatFrame");
-		liveChatFrame.src = `https://www.youtube.com/live_chat?is_popout=1&v=${videoId}`;
-	} else {
-		alert(
-			'Invalid YouTube link. It should start with "https://www.youtube.com/watch?v="'
-		);
-	}
+function embedVideo() {
+	var videoUrl = document.getElementById("videoUrl").value;
+	var videoId = extractVideoId(videoUrl);
+	var watchUrl = "watch.html?v=" + videoId;
+	window.location.href = watchUrl;
 }
 
-const watchButton = document.getElementById("watchButton");
-watchButton.addEventListener("click", parseLink);
+function extractVideoId(url) {
+	var videoId = url.split("v=")[1];
+	var ampersandPosition = videoId.indexOf("&");
+	if (ampersandPosition !== -1) {
+		videoId = videoId.substring(0, ampersandPosition);
+	}
+	return videoId;
+}
+
+function newStream() {
+	window.location.href = "index.html";
+}
